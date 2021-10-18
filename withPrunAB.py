a7447e6fecbdd691bb57c6414dc9d2316827e29e
Manue Ortiz Hernández <manuoh09@gmail.com>
2021-10-18 15:38:31 -0500

    Update withPrunAB.py

--- withPrunAB.py
+++ withPrunAB.py
@@ -1,2 +1,20 @@
+from board import Board
+
+
+
+
+def minimaxAB(board, depth, alpha, beta, actual_moves):
+	
+	if board.isFilled(actual_moves):
+		board.getBoard()
+	
+
+def main():
+	print("Tic-Tac-Toe con algoritmo minmax/Poda AlfaBeta")
+	board = Board()
+	print(board.print())
+	minimaxAB(board, 10, -1000, 1000, 8)
+
+
 if __name__ == '__main__':
-	print("hpola")
+	main()

7b9a9376a153a926be49e49480f570e747121110
Manue Ortiz Hernández <manuoh09@gmail.com>
2021-10-19 13:42:25 -0500

    Comments added

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ class User(Player):
         self.turns_counter+=1
         self.turn = not self.turn
 
-    def playUser(self, board):
+    def play(self, board):
         decision = input('¿En qué casilla desea jugar?: ')
         while not board.isValid(decision):
             print('Esa casilla no es válida.')
@@ -28,10 +28,8 @@ class AI(Player):
     def __init__(self, user_mark):
         self.alpha = -1000
         self.beta = 1000
-        self.optimal_val = -1000 
-        self.isMax = True
         super().__init__(X if user_mark == O else O)
-        if self.mark == X:
-            self.isMax = False
+        self.isMax = True if self.mark == O else False
+   
     
         
